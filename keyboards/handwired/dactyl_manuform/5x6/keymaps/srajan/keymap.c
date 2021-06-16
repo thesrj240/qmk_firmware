33249c7e8ea4dc5c5da11312ce2dd355d24387bb
Srajan Jain <Srajan@TheSrJMachine.bits-pilani.ac.in>
2021-06-16 23:19:24 +0530

    enhanced layer lighting logic

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/keymap.c
@@ -15,57 +15,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool led_enabled_by_layer;
+uint8_t previous_animation;
 
 void keyboard_post_init_user(void) {
   // Call the post init code.
   rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+  previous_animation = RGBLIGHT_MODE_SNAKE;
   led_enabled_by_layer = false;
-  rgblight_sethsv_noeeprom(HSV_MAGENTA); // sets the color to teal/cyan without saving
-  rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE); // sets mode to Fast breathing without saving
+  rgblight_sethsv_noeeprom(HSV_MAGENTA);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
 }
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
       case 4:   // reset
-          rgblight_sethsv_noeeprom(HSV_RED);
-          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
           if(!rgblight_is_enabled()){
             rgblight_toggle_noeeprom();
             led_enabled_by_layer = true;
           }
+          rgblight_sethsv_noeeprom(HSV_RED);
+          previous_animation = rgblight_get_mode();
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
           break;
       case 3:   // mouse
-          rgblight_sethsv_noeeprom(HSV_BLUE);
           if(!rgblight_is_enabled()){
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             rgblight_toggle_noeeprom();
+            previous_animation = rgblight_get_mode();
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             led_enabled_by_layer = true;
           }
+          rgblight_sethsv_noeeprom(HSV_BLUE);
           break;
       case 2:   // nav
-          rgblight_sethsv_noeeprom(HSV_ORANGE);
           if(!rgblight_is_enabled()){
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             rgblight_toggle_noeeprom();
+            previous_animation = rgblight_get_mode();
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             led_enabled_by_layer = true;
           }
+          rgblight_sethsv_noeeprom(HSV_ORANGE);
           break;
       case 1:   // symbols
-          rgblight_sethsv_noeeprom(HSV_PURPLE);
           if(!rgblight_is_enabled()){
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             rgblight_toggle_noeeprom();
+            previous_animation = rgblight_get_mode();
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             led_enabled_by_layer = true;
           }
+          rgblight_sethsv_noeeprom(HSV_PURPLE);
           break;
       default: //  for any other layers, or the default layer
           if(rgblight_is_enabled() && led_enabled_by_layer){
+            rgblight_mode_noeeprom(previous_animation);
+            rgblight_sethsv_noeeprom(HSV_MAGENTA);
             rgblight_toggle_noeeprom();
             led_enabled_by_layer = false;
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
           }
-          rgblight_sethsv_noeeprom(HSV_MAGENTA);
+          else{
+          	rgblight_mode_noeeprom(previous_animation);
+          	rgblight_sethsv_noeeprom(HSV_MAGENTA);
+          }
           break;
   }
   return state;
@@ -76,7 +86,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case RGB_TOG:
       if (record->event.pressed) {
-        rgblight_toggle_noeeprom();
+      	if(led_enabled_by_layer){
+      		rgblight_disable_noeeprom();
+      		led_enabled_by_layer = false;
+      	}
+      	else{
+        	rgblight_toggle_noeeprom();
+    	}
       }
       return false; // Skip all further processing of this key
 
