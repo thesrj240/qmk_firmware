d02dc80ef9691cea525d0265a64997d860b568bc
Srajan Jain <Srajan@TheSrJMachine.bits-pilani.ac.in>
2021-06-16 12:42:45 +0530

    fixing compilation issues and indentation

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/keymap.c
@@ -15,98 +15,98 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void keyboard_post_init_user(void) {
-  // Call the post init code.
-  rgblight_enable_noeeprom(); // enables Rgb, without saving settings
-  rgblight_sethsv_noeeprom(HSV_MAGENTA); // sets the color to teal/cyan without saving
-  rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE); // sets mode to Fast breathing without saving
+	// Call the post init code.
+	rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+	rgblight_sethsv_noeeprom(HSV_MAGENTA); // sets the color to teal/cyan without saving
+	rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE); // sets mode to Fast breathing without saving
 }
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  // enhance with logic of checking if RGB is enabled or not and handle accordingly
-  // see if mode needs to be set or not
-    switch (get_highest_layer(state)) {
-    case 4:   // reset
-        rgblight_sethsv_noeeprom(HSV_RED);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
-        break;
-    case 3:   // mouse
-        rgblight_sethsv_noeeprom(HSV_BLUE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    case 2:   // nav
-        rgblight_sethsv_noeeprom(HSV_ORANGE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    case 1:   // symbols
-        rgblight_sethsv_noeeprom(HSV_PURPLE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_sethsv_noeeprom(HSV_MAGENTA);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    }
-  return state;
+	// enhance with logic of checking if RGB is enabled or not and handle accordingly
+	// see if mode needs to be set or not
+	switch (get_highest_layer(state)) {
+	    case 4:   // reset
+	        rgblight_sethsv_noeeprom(HSV_RED);
+	        rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+	        break;
+	    case 3:   // mouse
+	        rgblight_sethsv_noeeprom(HSV_BLUE);
+	        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+	        break;
+	    case 2:   // nav
+	        rgblight_sethsv_noeeprom(HSV_ORANGE);
+	        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+	        break;
+	    case 1:   // symbols
+	        rgblight_sethsv_noeeprom(HSV_PURPLE);
+	        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+	        break;
+	    default: //  for any other layers, or the default layer
+	        rgblight_sethsv_noeeprom(HSV_MAGENTA);
+	        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+	        break;
+	    }
+	return state;
 }
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case RGB_TOG:
-      if (record->event.pressed) {
-        rgblight_toggle_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+	switch (keycode) {
+		case RGB_TOG:
+			if (record->event.pressed) {
+				rgblight_toggle_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_MODE_FORWARD:
-      if (record->event.pressed) {
-        rgblight_step_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_MODE_FORWARD:
+			if (record->event.pressed) {
+				rgblight_step_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_MODE_REVERSE:
-      if (record->event.pressed) {
-        rgblight_step_reverse_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_MODE_REVERSE:
+			if (record->event.pressed) {
+				rgblight_step_reverse_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_HUI:
-      if (record->event.pressed) {
-        rgblight_increase_hue_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_HUI:
+			if (record->event.pressed) {
+				rgblight_increase_hue_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_HUD:
-      if (record->event.pressed) {
-        rgblight_decrease_hue_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_HUD:
+			if (record->event.pressed) {
+				rgblight_decrease_hue_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_SAI:
-      if (record->event.pressed) {
-        rgblight_increase_sat_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_SAI:
+			if (record->event.pressed) {
+				rgblight_increase_sat_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_SAD:
-      if (record->event.pressed) {
-        rgblight_decrease_sat_noeeprom();
-      return false; // Skip all further processing of this key
+		case RGB_SAD:
+			if (record->event.pressed) {
+				rgblight_decrease_sat_noeeprom();
+			return false; // Skip all further processing of this key
 
-      case RGB_VAI:
-      if (record->event.pressed) {
-        rgblight_increase_val_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_VAI:
+			if (record->event.pressed) {
+				rgblight_increase_val_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-      case RGB_VAD:
-      if (record->event.pressed) {
-        rgblight_decrease_val_noeeprom();
-      }
-      return false; // Skip all further processing of this key
+		case RGB_VAD:
+			if (record->event.pressed) {
+				rgblight_decrease_val_noeeprom();
+			}
+			return false; // Skip all further processing of this key
 
-    default:
-      return true; // Process all other keycodes normally
-  }
+		default:
+			return true; // Process all other keycodes normally
+	}
 }
