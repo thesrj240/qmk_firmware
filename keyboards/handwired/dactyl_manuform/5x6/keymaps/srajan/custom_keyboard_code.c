4dc8ca4edbbad4c5cbbc433474c88c4aa3286930
Srajan Jain <Srajan@TheSrJMachine.bits-pilani.ac.in>
2021-06-16 16:19:33 +0530

    enhanced layer lighting logic

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/custom_keyboard_code.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/srajan/custom_keyboard_code.c
@@ -1,36 +1,57 @@
+bool led_enabled_by_layer;
+
 void keyboard_post_init_user(void) {
   // Call the post init code.
   rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+  led_enabled_by_layer = false;
   rgblight_sethsv_noeeprom(HSV_MAGENTA); // sets the color to teal/cyan without saving
   rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE); // sets mode to Fast breathing without saving
 }
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  // enhance with logic of checking if RGB is enabled or not and handle accordingly
-  // see if mode needs to be set or not
-    switch (get_highest_layer(state)) {
-    case 4:   // reset
-        rgblight_sethsv_noeeprom(HSV_RED);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
-        break;
-    case 3:   // mouse
-        rgblight_sethsv_noeeprom(HSV_BLUE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    case 2:   // nav
-        rgblight_sethsv_noeeprom(HSV_ORANGE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    case 1:   // symbols
-        rgblight_sethsv_noeeprom(HSV_PURPLE);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_sethsv_noeeprom(HSV_MAGENTA);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
-        break;
-    }
+  switch (get_highest_layer(state)) {
+      case 4:   // reset
+          rgblight_sethsv_noeeprom(HSV_RED);
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+          if(!rgblight_is_enabled()){
+            rgblight_toggle_noeeprom();
+            led_enabled_by_layer = true;
+          }
+          break;
+      case 3:   // mouse
+          rgblight_sethsv_noeeprom(HSV_BLUE);
+          if(!rgblight_is_enabled()){
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_toggle_noeeprom();
+            led_enabled_by_layer = true;
+          }
+          break;
+      case 2:   // nav
+          rgblight_sethsv_noeeprom(HSV_ORANGE);
+          if(!rgblight_is_enabled()){
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_toggle_noeeprom();
+            led_enabled_by_layer = true;
+          }
+          break;
+      case 1:   // symbols
+          rgblight_sethsv_noeeprom(HSV_PURPLE);
+          if(!rgblight_is_enabled()){
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_toggle_noeeprom();
+            led_enabled_by_layer = true;
+          }
+          break;
+      default: //  for any other layers, or the default layer
+          if(rgblight_is_enabled() && led_enabled_by_layer){
+            rgblight_toggle_noeeprom();
+            led_enabled_by_layer = false;
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+          }
+          rgblight_sethsv_noeeprom(HSV_MAGENTA);
+          break;
+  }
   return state;
 }
 
@@ -43,48 +64,49 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false; // Skip all further processing of this key
 
-      case RGB_MODE_FORWARD:
+    case RGB_MODE_FORWARD:
       if (record->event.pressed) {
         rgblight_step_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_MODE_REVERSE:
+    case RGB_MODE_REVERSE:
       if (record->event.pressed) {
         rgblight_step_reverse_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_HUI:
+    case RGB_HUI:
       if (record->event.pressed) {
         rgblight_increase_hue_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_HUD:
+    case RGB_HUD:
       if (record->event.pressed) {
         rgblight_decrease_hue_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_SAI:
+    case RGB_SAI:
       if (record->event.pressed) {
         rgblight_increase_sat_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_SAD:
+    case RGB_SAD:
       if (record->event.pressed) {
         rgblight_decrease_sat_noeeprom();
+      }
       return false; // Skip all further processing of this key
 
-      case RGB_VAI:
+    case RGB_VAI:
       if (record->event.pressed) {
         rgblight_increase_val_noeeprom();
       }
       return false; // Skip all further processing of this key
 
-      case RGB_VAD:
+    case RGB_VAD:
       if (record->event.pressed) {
         rgblight_decrease_val_noeeprom();
       }
