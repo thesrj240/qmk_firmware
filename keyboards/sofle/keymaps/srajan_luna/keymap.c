9b39462c2a16f75602648db66ea6dd83c0e144f4
thesrj240 <sr.j240@gmail.com>
2022-05-07 20:04:45 +0530

    minor changes

--- keyboards/sofle/keymaps/srajan_luna/keymap.c
+++ keyboards/sofle/keymaps/srajan_luna/keymap.c
@@ -432,15 +432,21 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
           if(scroll_vertically){
             if (clockwise) {
               tap_code(KC_WH_D);
+              tap_code(KC_WH_D);
+              tap_code(KC_WH_D);
             } else {
+              tap_code(KC_WH_U);
+              tap_code(KC_WH_U);
               tap_code(KC_WH_U);
             }
           }
           else{
   					if (clockwise) {
   						tap_code(KC_WH_R);
+              tap_code(KC_WH_R);
   					} else {
   						tap_code(KC_WH_L);
+              tap_code(KC_WH_L);
   					}
           }
 				break;
